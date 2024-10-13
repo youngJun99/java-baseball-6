1e8f7d25c0c2e90202a86206618ada404f670bc0
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-13 16:28:54 +0900

    feat: 결과 view 개발

---
+++ src/main/java/baseball/view/resultView.java
@@ -0,0 +1,28 @@
+package baseball.view;
+
+import baseball.controller.InputManager;
+
+import java.util.List;
+
+public class resultView {
+
+    private static final String WIN_MESSAGE = "%d개의 숫자를 모두 맞히셨습니다! 게임종료";
+
+    public void printScore(List<Integer> answerResult){
+        int ball =answerResult.get(0);
+        int strike = answerResult.get(1);
+
+        if(ball == 0 && strike==0) {
+            System.out.println("낫싱");
+        }else if(strike == InputManager.GAME_LENGTH){
+            System.out.println(strike+"스트라이크");
+            System.out.println(String.format(WIN_MESSAGE,InputManager.GAME_LENGTH));
+        }else if(ball==0){
+            System.out.println(strike+"스트라이크");
+        }else if(strike==0){
+            System.out.println(ball+"볼");
+        }else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+    }
+}
