486a8d3a6825da531cafb3de16d6d1e100fda2b7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-13 16:32:15 +0900

    fix: ScoreValidator return값 선언

--- src/main/java/baseball/domain/game/ScoreValidator.java
+++ src/main/java/baseball/domain/game/ScoreValidator.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 public class ScoreValidator {
 
-    public List<Integer> validateAnswer(List<Integer> answer ,List<Integer> guess){
+    public List<Integer> validateAnswer(List<Integer> answer, List<Integer> guess) {
 
-        List<Integer> answerResult = List.of(0,0);
+        List<Integer> answerResult = List.of(0, 0);
 
-        for(int i=0; i<answer.size(); i++){
-            if(answer.get(i)==guess.get(i)) answerResult.set(1, answerResult.get(1) + 1);
-            for(int j=0; j<answer.size(); j++){
-                if(answer.get(i)==guess.get(j) && i!=j) answerResult.set(0, answerResult.get(0) + 1);
+        for (int i = 0; i < answer.size(); i++) {
+            if (answer.get(i) == guess.get(i)) answerResult.set(1, answerResult.get(1) + 1);
+            for (int j = 0; j < answer.size(); j++) {
+                if (answer.get(i) == guess.get(j) && i != j) answerResult.set(0, answerResult.get(0) + 1);
             }
         }
+        return answerResult;
     }
 }
