1e71c44ac13372cbad51f824aa79237d2fea832b
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-13 16:18:08 +0900

    feat: ScoreValidator 점수 계산 로직 구현

---
+++ src/main/java/baseball/domain/game/ScoreValidator.java
@@ -0,0 +1,19 @@
+package baseball.domain.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScoreValidator {
+
+    public List<Integer> validateAnswer(List<Integer> answer ,List<Integer> guess){
+
+        List<Integer> answerResult = List.of(0,0);
+
+        for(int i=0; i<answer.size(); i++){
+            if(answer.get(i)==guess.get(i)) answerResult.set(1, answerResult.get(1) + 1);
+            for(int j=0; j<answer.size(); j++){
+                if(answer.get(i)==guess.get(j) && i!=j) answerResult.set(0, answerResult.get(0) + 1);
+            }
+        }
+    }
+}
