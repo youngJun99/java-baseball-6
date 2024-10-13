69e1ac45d9055861399a2f62a5115a938b390315
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-13 16:53:43 +0900

    fix: 문법 수정

--- src/main/java/baseball/domain/game/ScoreValidator.java
+++ src/main/java/baseball/domain/game/ScoreValidator.java
@@ -10,9 +10,11 @@ public class ScoreValidator {
         List<Integer> answerResult = List.of(0, 0);
 
         for (int i = 0; i < answer.size(); i++) {
-            if (answer.get(i) == guess.get(i)) answerResult.set(1, answerResult.get(1) + 1);
+            if (answer.get(i) == guess.get(i))
+                answerResult.set(1, answerResult.get(1) + 1);
             for (int j = 0; j < answer.size(); j++) {
-                if (answer.get(i) == guess.get(j) && i != j) answerResult.set(0, answerResult.get(0) + 1);
+                if (answer.get(i) == guess.get(j) && i != j)
+                    answerResult.set(0, answerResult.get(0) + 1);
             }
         }
         return answerResult;
