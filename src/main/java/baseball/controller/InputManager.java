8688eccd631eb39e6d6469d06a189e9b3b2f1168
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-13 16:08:27 +0900

    feat: InputManager를 통해서 입력받는 숫자에 대한 검증과 메시지 구현

--- src/main/java/baseball/controller/InputManager.java
+++ src/main/java/baseball/controller/InputManager.java
@@ -11,7 +11,7 @@ public class InputManager {
     private final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다";
     private final static String ANSWER_RECIEVE_MESSAGE = "숫자를 입력해 주세요: ";
     private final static String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static int GAME_LENGTH = 3;
+    public static int GAME_LENGTH = 3;
 
     public void printGameStartMessage(){
         System.out.println(GAME_START_MESSAGE);
