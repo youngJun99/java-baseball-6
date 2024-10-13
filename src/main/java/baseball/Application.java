7d7b48f5de471dd10eaa82f3920e1b18841adca6
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-13 17:01:03 +0900

    fix: GameController에 새로운 프러퍼티 추가함에 따라 의존성 주입 요소 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import baseball.controller.GameController;
 import baseball.controller.InputManager;
 import baseball.domain.game.RandomNumberGenarator;
 import baseball.domain.game.ScoreValidator;
+import baseball.domain.game.WinValidator;
 import baseball.view.ResultView;
 
 public class Application {
@@ -12,7 +13,8 @@ public class Application {
                 new InputManager(),
                 new RandomNumberGenarator(),
                 new ScoreValidator(),
-                new ResultView());
+                new ResultView(),
+                new WinValidator());
 
         gameController.play();
 
